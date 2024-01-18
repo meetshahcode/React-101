f2ee59536a281f09cc92834dd92ea059772cff20
Meet Shah <meet.shah@begenuin.com>
2024-01-18 18:36:19 +0530

    post project milestone 1 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import PostList from "../src/components/PostList";
 import MainHeader from "../src/components/MainHeader";
 
 function App() {
+  const [modalVisibleState, setModalVisibeState] = useState(false);
+
+  function hideModalHandler() {
+    setModalVisibeState(false);
+  }
+
+  function showModalHandler() {
+    setModalVisibeState(true);
+  }
   return (
     <>
       <header>
-        <MainHeader />
+        <MainHeader onCreatePost={showModalHandler} />
       </header>
       <main>
-        <PostList />
+        <PostList
+          onClosePosting={hideModalHandler}
+          isPosting={modalVisibleState}
+        />
       </main>
     </>
   );
